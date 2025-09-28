ca777b06e362223466446893d2c08333141da07a
Mario Casas <MCasas548@gmail.com>
2025-09-28 07:57:57 -0400

    commit

--- src/game/scenes/QuestMenu.ts
+++ src/game/scenes/QuestMenu.ts
@@ -3,9 +3,6 @@ import { fetchFacts, fetchBiodiversity, fetchNewDay, fetchQuestDirect } from '@/
 
 export default class QuestMenu extends Phaser.Scene {
 
-
-    public static quest: Quest;
-    public static questUpdated: boolean;
 constructor() {
         super({ key: 'QuestMenu' });
     }
@@ -15,7 +12,6 @@ constructor() {
     }
 
     create() {
-        QuestMenu.questUpdated = false;
         const centerX = this.sys.game.config.width as number / 2;
         const centerY = this.sys.game.config.height as number / 2;
         // Background
